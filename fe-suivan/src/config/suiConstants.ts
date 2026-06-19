7ff2e47e3dccfa3ae0292500854905c4e258f97a
Ozan_OnChain <yt2025id@gmail.com>
2026-06-19 13:45:20 +0700

    feat: update to new testnet contract deployment (Faiz)

--- fe-suivan/src/config/suiConstants.ts
+++ fe-suivan/src/config/suiConstants.ts
@@ -1,6 +1,6 @@
-export const SUI_PACKAGE_ID = "0xb79c6171ac1ce89d864f1ce59329b8393d7f540e6e31b30cad0b71c54729bfb6";
-export const SUI_FACTORY_ID = "0x70a934372b9508ca92e8b0ed11ca4bfb0a42d17d27c6fb7838f195b5cc74714d";
+export const SUI_PACKAGE_ID = "0x63ad9b5fb0fa7f286ac05892182e4eb5896cc9165f9bd2b7d0ba1de87b81b515";
+export const SUI_FACTORY_ID = "0x4484b70fdea8a4aefcfef9c6a33e13d975b2cde0ce6a2085cb8eb18cf5e6af32";
 export const SUI_USDC_TYPE = `${SUI_PACKAGE_ID}::test_usdc::TEST_USDC`;
 export const SUI_SUI_TYPE = `${SUI_PACKAGE_ID}::test_sui::TEST_SUI`;
 export const SUI_CLOCK_ID = "0x6";
-export const SUI_AGENT_ADDRESS = "0x0c605e48372c48178372abb66f4444ee3d25fe954bd9010879b6c9797e6ef061";
+export const SUI_AGENT_ADDRESS = "0x501f2840d1d6fb2a98299f52f671150d38e118c33e8342861dd4ad5d58b788f1";
